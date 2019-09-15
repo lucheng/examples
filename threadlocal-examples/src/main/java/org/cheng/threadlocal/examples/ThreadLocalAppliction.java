a232c7aacf287c3e08af86a12dddf06aa66090e7
lucheng <lucheng@192.168.56.1>
2019-09-15 16:18:25 +0800

    新增ThreadLocal

---
+++ threadlocal-examples/src/main/java/org/cheng/threadlocal/examples/ThreadLocalAppliction.java
@@ -0,0 +1,11 @@
+package org.cheng.threadlocal.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLocalAppliction {
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadLocalAppliction.class, args);
+	}
+}
