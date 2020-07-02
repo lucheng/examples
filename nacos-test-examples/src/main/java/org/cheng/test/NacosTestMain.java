71169b0ee4e52a3e40bb3247e8c3ab56ad706219
lucheng <1213136330@qq.com>
2020-07-02 12:16:49 +0800

    添加nacos配置中心的测试

---
+++ nacos-test-examples/src/main/java/org/cheng/test/NacosTestMain.java
@@ -0,0 +1,13 @@
+package org.cheng.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NacosTestMain {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(NacosTestMain.class, args);
+    }
+}
